f0d75bf3c36cceba1ca735dcdccdb8a46dbb412a
dranzerAvi <aviralhtc2015@gmail.com>
2020-07-03 05:11:36 +0530

    Basic card swipe UI done🤘

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jabwemate/Screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
