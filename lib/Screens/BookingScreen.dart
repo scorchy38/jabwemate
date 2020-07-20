251b0c2538de3df5cbec48de406149c3a6f773f0
unknown <shreyas.ny@gmail.com>
2020-07-21 02:30:26 +0530

    Doc List Screen added

---
+++ lib/Screens/BookingScreen.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:jabwemate/Classes/Doc_data.dart';
+import 'package:jabwemate/Widgets/docCustomDrawer.dart';
+import 'package:jabwemate/Widgets/appbar.dart';
+import 'package:jabwemate/Screens/docMainScreen.dart';
+
+class BookingScreen extends StatefulWidget {
+  @override
+  _BookingScreenState createState() => _BookingScreenState();
+}
+
+class _BookingScreenState extends State<BookingScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/Screens/docMainScreen.dart
+++ lib/Screens/docMainScreen.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:jabwemate/Classes/Doc_data.dart';
 import 'package:jabwemate/Widgets/docCustomDrawer.dart';
 import 'package:jabwemate/Widgets/appbar.dart';
+import 'package:jabwemate/Screens/BookingScreen.dart';
 
 class DocMainScreen extends StatefulWidget {
   @override
@@ -61,6 +63,92 @@ class _DocMainScreenState extends State<DocMainScreen> {
     super.initState();
   }
 
+  //function to return list of column of doctors
+  _builddocotor() {
+    List<Widget> docList = [];
+
+    //going through every element in the list (docpros present in Doc_data in classes)
+    docpros.forEach((Docpro docpro) {
+      docList.add(GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => BookingScreen(), //sending data to BookingScreen
+          ),
+        ),
+        child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
+                border: Border.all(
+                  width: 1.0,
+                  color: Colors.grey[200],
+                )),
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    height: 150.0,
+                    width: 150.0,
+                    image: AssetImage(docpro.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      margin: EdgeInsets.all(12.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            docpro.name,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 4.0),
+                          Text(
+                            docpro.specs,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 4.0),
+                          Text(
+                            docpro.degr,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 4.0),
+                          Text(
+                            docpro.address,
+                            style: TextStyle(
+                              fontSize: 13.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 4.0),
+                        ],
+                      )),
+                )
+              ],
+            )),
+      ));
+    });
+    return (Column(children: docList));
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -70,27 +158,25 @@ class _DocMainScreenState extends State<DocMainScreen> {
       drawer: DocCustomDrawer(),
       backgroundColor: Color(0xFFEFF7F6),
       appBar: CustomAppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: ListView(
         children: <Widget>[
-          SizedBox(
-            height: height * 0.03,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Nearby Doctors',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ),
+              _builddocotor(),
+            ],
           ),
-          Text(
-            'Latest Profiles',
-            style: GoogleFonts.k2d(
-                fontSize: height * 0.035,
-                fontWeight: FontWeight.bold,
-                color: Colors.black.withOpacity(0.5)),
-          ),
-          Container(
-            alignment: Alignment.topCenter,
-            height: height * 0.15,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[],
-            ),
-          )
         ],
       ),
     );
