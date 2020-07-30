3168cafe53cca29f32c0ba9325d940b722b063f7
Chitransh Jain <cjsmcs2810@gmail.com>
2020-07-30 23:22:43 +0530

    inkwells changed.

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tindercard/flutter_tindercard.dart';
-
 import 'package:getflutter/getflutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jabwemate/Classes/dog_profile.dart';
