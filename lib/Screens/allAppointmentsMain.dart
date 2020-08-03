ceed125a938e1334462ce0e5f9282f5729538e07
Rhythm Gupta <rhythmgupta1234@gmail.com>
2020-08-03 19:29:08 +0530

    Cancellation error fixed

--- lib/Screens/allAppointmentsMain.dart
+++ lib/Screens/allAppointmentsMain.dart
@@ -5,6 +5,8 @@ import 'package:jabwemate/Screens/appointmentPast.dart';
 import 'package:jabwemate/Widgets/appbar.dart';
 import 'package:jabwemate/Screens/appointmentFuture.dart';
 
+import 'docMainScreen.dart';
+
 class AppointmentMainPage extends StatefulWidget {
   @override
   _AppointmentMainPageState createState() => _AppointmentMainPageState();
@@ -13,83 +15,97 @@ class AppointmentMainPage extends StatefulWidget {
 class _AppointmentMainPageState extends State<AppointmentMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-          // action: IconButton(
-          //   onPressed: () {
-          //     showDialog(
-          //         context: context,
-          //         builder: (BuildContext context) {
-          //           return AlertDialog(
-          //             title: Text("Add a dog"),
-          //             actions: [
-          //               FlatButton(
-          //                 child: Text("For Adoption"),
-          //                 onPressed: () {
-          //                   Navigator.of(context).push(MaterialPageRoute(
-          //                       builder: (BuildContext context) =>
-          //                           AddDogAdopt()));
-          //                 },
-          //               ),
-          //               FlatButton(
-          //                 child: Text("For sale"),
-          //                 onPressed: () {
-          //                   Navigator.of(context).push(MaterialPageRoute(
-          //                       builder: (BuildContext context) => AddDogSell()));
-          //                 },
-          //               )
-          //             ],
-          //           );
-          //         });
-          //   },
-          //   icon: Icon(
-          //     Icons.add,
-          //     color: Colors.white,
-          //   ),
-          // ),
+    return WillPopScope(
+      onWillPop: () {
+        int count = 0;
+        Navigator.popUntil(context, (route) {
+          return count++ == 3;
+        });
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DocMainScreen(),
           ),
-      backgroundColor: Colors.white,
-      body: SafeArea(
-          child: Container(
-        height: double.infinity,
-        child: GFTabs(
-          tabBarColor: Color(0xFFf7418c),
-          tabBarHeight: 70,
-          indicatorColor: Colors.white,
+        );
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(
+            // action: IconButton(
+            //   onPressed: () {
+            //     showDialog(
+            //         context: context,
+            //         builder: (BuildContext context) {
+            //           return AlertDialog(
+            //             title: Text("Add a dog"),
+            //             actions: [
+            //               FlatButton(
+            //                 child: Text("For Adoption"),
+            //                 onPressed: () {
+            //                   Navigator.of(context).push(MaterialPageRoute(
+            //                       builder: (BuildContext context) =>
+            //                           AddDogAdopt()));
+            //                 },
+            //               ),
+            //               FlatButton(
+            //                 child: Text("For sale"),
+            //                 onPressed: () {
+            //                   Navigator.of(context).push(MaterialPageRoute(
+            //                       builder: (BuildContext context) => AddDogSell()));
+            //                 },
+            //               )
+            //             ],
+            //           );
+            //         });
+            //   },
+            //   icon: Icon(
+            //     Icons.add,
+            //     color: Colors.white,
+            //   ),
+            // ),
+            ),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Container(
+          height: double.infinity,
+          child: GFTabs(
+            tabBarColor: Color(0xFFf7418c),
+            tabBarHeight: 70,
+            indicatorColor: Colors.white,
 
-          initialIndex: 0,
-          length: 2,
+            initialIndex: 0,
+            length: 2,
 //      shape: RoundedRectangleBorder(
 //          borderRadius: BorderRadius.only(
 //              bottomRight: Radius.circular(10),
 //              bottomLeft: Radius.circular(10))),
-          tabs: <Widget>[
-            Tab(
-              child: Text(
-                "Coming Up",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "nunito",
+            tabs: <Widget>[
+              Tab(
+                child: Text(
+                  "Coming Up",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "nunito",
+                  ),
                 ),
               ),
-            ),
-            Tab(
-              child: Text(
-                "Past",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "nunito",
+              Tab(
+                child: Text(
+                  "Past",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "nunito",
+                  ),
                 ),
               ),
+            ],
+            tabBarView: GFTabBarView(
+              children: <Widget>[FutureAppointment(), PastAppointment()],
             ),
-          ],
-          tabBarView: GFTabBarView(
-            children: <Widget>[FutureAppointment(), PastAppointment()],
           ),
-        ),
-      )),
+        )),
+      ),
     );
   }
 }
