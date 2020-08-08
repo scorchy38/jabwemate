5cbc273751c1b1d9ecccdc493f3e9cafc1326202
Chitransh Jain <cjsmcs2810@gmail.com>
2020-08-08 14:52:34 +0530

    fixes.

--- lib/e-commerce_module/OtherPages/CartPage.dart
+++ lib/e-commerce_module/OtherPages/CartPage.dart
@@ -415,131 +415,8 @@ class _CartPageState extends State<CartPage> {
                         height: MediaQuery.of(context).size.height / 80,
                       ),
                       InkWell(
-                        onTap: () {
-                          Alert(
-                              context: context,
-                              title: "PROCEED TO CHECKOUT",
-                              content: Form(
-                                key: formKey,
-                                child: Column(
-                                  children: <Widget>[
-                                    TextFormField(
-                                      validator: (value) {
-                                        if (value.length == 0) {
-                                          return 'Please enter your name';
-                                        } else {
-                                          return null;
-                                        }
-                                      },
-                                      controller: nameController,
-                                      decoration: InputDecoration(
-                                        icon: Icon(Icons.account_circle),
-                                        labelText: 'Name',
-                                      ),
-                                    ),
-                                    TextFormField(
-                                      validator: (value) {
-                                        if (value.length < 10) {
-                                          return 'Please enter a valid phone number';
-                                        } else {
-                                          return null;
-                                        }
-                                      },
-                                      controller: numberController,
-                                      decoration: InputDecoration(
-                                        icon: Icon(Icons.phone_iphone),
-                                        labelText: 'Contact Number',
-                                      ),
-                                    ),
-                                    TextFormField(
-                                      validator: (value) {
-                                        if (value.length == 0) {
-                                          return 'Please enter your address';
-                                        } else {
-                                          return null;
-                                        }
-                                      },
-                                      controller: addressController,
-                                      decoration: InputDecoration(
-                                        icon: Icon(Icons.home),
-                                        labelText: 'Address',
-                                      ),
-                                    ),
-                                    TextFormField(
-                                      validator: (value) {
-                                        if (value.length < 6) {
-                                          return 'Please enter a valid pin code';
-                                        } else {
-                                          return null;
-                                        }
-                                      },
-                                      controller: zipController,
-                                      decoration: InputDecoration(
-                                        icon: Icon(Icons.location_on),
-                                        labelText: 'PIN Code',
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: pWidth * 0.05,
-                                    ),
-                                    Text(
-                                      "Order Total : Rs. ${(totalAmount() + (0.18 * totalAmount()) + 00).toStringAsFixed(2)}",
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'Cabin',
-                                          fontSize: MediaQuery.of(context)
-                                                  .size
-                                                  .height /
-                                              50,
-                                          color: kPrimaryColor),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              buttons: [
-                                DialogButton(
-                                  onPressed: () {
-                                    if (formKey.currentState.validate()) {
-                                      Navigator.pop(context);
-                                      onOrderPlaced();
-                                    }
-                                  },
-                                  color: kPrimaryColor,
-                                  child: Text(
-                                    "PROCEED FOR COD",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Cabin',
-                                        fontSize:
-                                            MediaQuery.of(context).size.height /
-                                                70,
-                                        color: Colors.white),
-                                  ),
-                                ),
-                                DialogButton(
-                                  onPressed: () async {
-                                    if (formKey.currentState.validate()) {
-                                      //openCheckout();
-                                      Navigator.pop(context);
-                                      onOrderPlaced();
-                                    }
-                                  },
-                                  color: kPrimaryColor,
-                                  child: Text(
-                                    "PAY ONLINE",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Cabin',
-                                        fontSize:
-                                            MediaQuery.of(context).size.height /
-                                                70,
-                                        color: Colors.white),
-                                  ),
-                                )
-                              ]).show();
+                        onTap: () async {
+                          await order(context, pWidth);
                         },
                         child: Container(
                           width: MediaQuery.of(context).size.height - 20,
@@ -657,11 +534,6 @@ class _CartPageState extends State<CartPage> {
     return sum;
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   void openCheckout() async {
     var options = {
       'key': 'abc',
@@ -696,5 +568,125 @@ class _CartPageState extends State<CartPage> {
   void _handleExternalWallet(ExternalWalletResponse response) {
     Fluttertoast.showToast(
         msg: "EXTERNAL_WALLET: " + response.walletName, timeInSecForIosWeb: 4);
+  }
+
+  void order(BuildContext context, double pWidth) {
+    Alert(
+        context: context,
+        title: "PROCEED TO CHECKOUT",
+        content: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (value) {
+                  if (value.length == 0) {
+                    return 'Please enter your name';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: nameController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.account_circle),
+                  labelText: 'Name',
+                ),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value.length < 10) {
+                    return 'Please enter a valid phone number';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: numberController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.phone_iphone),
+                  labelText: 'Contact Number',
+                ),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value.length == 0) {
+                    return 'Please enter your address';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: addressController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.home),
+                  labelText: 'Address',
+                ),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value.length < 6) {
+                    return 'Please enter a valid pin code';
+                  } else {
+                    return null;
+                  }
+                },
+                controller: zipController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.location_on),
+                  labelText: 'PIN Code',
+                ),
+              ),
+              SizedBox(
+                height: pWidth * 0.05,
+              ),
+              Text(
+                "Order Total : Rs. ${(totalAmount() + (0.18 * totalAmount()) + 00).toStringAsFixed(2)}",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Cabin',
+                    fontSize: MediaQuery.of(context).size.height / 50,
+                    color: kPrimaryColor),
+              )
+            ],
+          ),
+        ),
+        buttons: [
+          DialogButton(
+            onPressed: () {
+              if (formKey.currentState.validate()) {
+                Navigator.pop(context);
+                onOrderPlaced();
+              }
+            },
+            color: kPrimaryColor,
+            child: Text(
+              "PROCEED FOR COD",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Cabin',
+                  fontSize: MediaQuery.of(context).size.height / 70,
+                  color: Colors.white),
+            ),
+          ),
+          DialogButton(
+            onPressed: () async {
+              if (formKey.currentState.validate()) {
+                //openCheckout();
+                Navigator.pop(context);
+                onOrderPlaced();
+              }
+            },
+            color: kPrimaryColor,
+            child: Text(
+              "PAY ONLINE",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Cabin',
+                  fontSize: MediaQuery.of(context).size.height / 70,
+                  color: Colors.white),
+            ),
+          )
+        ]).show();
   }
 }
